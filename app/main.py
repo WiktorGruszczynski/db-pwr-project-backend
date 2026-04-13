47f4e9ab8c6deb0474a21dc6a426dc5c57f8d6dd
Janek <janwieczorek1805@gmail.com>
2026-04-13 15:03:18 +0200

    refactor: rename certain variables (forgot before)

--- app/main.py
+++ app/main.py
@@ -2,7 +2,7 @@ from dotenv import load_dotenv
 from fastapi import FastAPI, Depends
 from fastapi.responses import FileResponse
 from app.database import get_db_connection
-from app.auth.router import router as auth_router
+from app.users.router import router as users_router
 
 load_dotenv()
 
@@ -20,4 +20,4 @@ def root(db=Depends(get_db_connection)):
     return {"message": db.cursor()}
 
 
-app.include_router(auth_router)
+app.include_router(users_router)
